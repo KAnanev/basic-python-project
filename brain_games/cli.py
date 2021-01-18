9b2dc9d5e818d1b10fc43d82c08942a46f8e4ef2
russrage <russrage@gmail.com>
2021-01-19 00:03:58 +0300

    step 4

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,5 +1,10 @@
+"""Add function welcome_user."""
+import sys
+
 import prompt
 
+
 def welcome_user():
+    """Add Function."""
     name = prompt.string('May I have your name? ')
-    print(f'Hello, {name}')
+    sys.stdout.write('Hello, {user}!'.format(user=name))
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python
+"""Main program."""
+
+import sys
 
 from brain_games import cli
 
-def main():
-    print('Welcome to the Brain Games!')
-    cli.welcome_user()
 
+def main():
+    """Add main function."""
+    sys.stdout.write('Welcome to the Brain Games!')
+    cli.welcome_user()
 
 
 if __name__ == '__main__':
