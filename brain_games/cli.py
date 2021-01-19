0ec11eb4e9d4c8649d34cf94f1e36409076bdc59
russrage <russrage@gmail.com>
2021-01-20 01:47:44 +0300

    Stable 0.1.3

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -15,5 +15,27 @@ def welcome_user():
     return name
 
 
-if __name__ == '__main__':
-    welcome_user()
+def decorator_game(func):
+    """Add decorator for game.
+
+    Returns:
+        wrapper.
+
+    Parameter:
+        func: name.
+    """
+    def wrapper_game(name):
+        count = 0
+        while count != 3:
+            result_game = func(name)
+            if result_game[0] == result_game[1]:
+                sys.stdout.write('Correct!\n')
+                count += 1
+            else:
+                sys.stdout.write(
+                    "Let's try again, {user}!\n".format(user=name),
+                )
+                break
+        if count == 3:
+            sys.stdout.write('Congratulations, {user}!\n'.format(user=name))
+    return wrapper_game
--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -4,45 +4,28 @@ import sys
 
 import prompt
 
+from brain_games.cli import decorator_game
 from brain_games.scripts import brain_games
 
 
-def check_even(num):
-    """Add function even.
-
-    Args:
-        num: number.
-
-    Returns:
-        yes or no.
-    """
-    return 'yes' if num % 2 == 0 else 'no'
-
-
+@decorator_game
 def check_parity(name):
     """Add description function.
 
     Args:
         name: name.
+
+    Returns:
+        variables.
     """
-    count = 0
-    while count != 3:
-        question = secrets.randbelow(100)
-        sys.stdout.write(
-            'Answer "yes" if a number is even, otherwise answer "no".\n',
-        )
-        sys.stdout.write('Question: {num}\n'.format(num=question))
-        answer = prompt.string('Your answer: ')
-
-        if check_even(question) == answer:
-            sys.stdout.write('Correct!\n')
-            count += 1
-        else:
-            sys.stdout.write('Let"s try again, {user}!\n'.format(user=name))
-            break
-
-    if count == 3:
-        sys.stdout.write('Congratulations, {user}!\n'.format(user=name))
+    question = secrets.randbelow(100)
+    check_even = 'yes' if question % 2 == 0 else 'no'
+    sys.stdout.write(
+        'Answer "yes" if a number is even, otherwise answer "no".\n',
+    )
+    sys.stdout.write('Question: {num}\n'.format(num=question))
+    answer = prompt.string('Your answer: ')
+    return check_even, answer
 
 
 def main():
