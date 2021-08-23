45e095773a3a43da188b9a6ddc0df11715f8dfe4
KAnanev <59393620+KAnanev@users.noreply.github.com>
2021-08-23 16:50:35 +0300

    Update brain_progression.py

--- brain_games/scripts/games/brain_progression.py
+++ brain_games/scripts/games/brain_progression.py
@@ -13,8 +13,10 @@ def list_nums():
     Returns:
         variables.
     """
-    max_num = 10
-    num = 10
+    random_num = secrets.SystemRandom()
+    
+    max_num = random_num.randrange(1, 11)
+    num = random_num.randrange(1, 11)
     num_start = secrets.randbelow(max_num)
     num_step = secrets.randbelow(num)
     return list(range(num_start, 100, num_step))[:10]
